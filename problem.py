0e63c2989d7f9c7f916ef7f9f901074a75e7f99b
Khairalla <161679719+Mahmoudkhairalla@users.noreply.github.com>
2024-03-29 14:59:45 +0700

    first

---
+++ problem.py
@@ -0,0 +1,17 @@
+print("""We have diffrent items from cosmatics\nyou can take the items by just 10%
+      Real prices for items 
+      item 1 = 540
+      item 2 = 720
+      item 3 = 850""")
+powder = 540 * 10 / 100
+Eyeshadow = 720 * 10 / 100
+lipstick = 850 * 10 / 100
+choice = int(input("type the number of the item  and get the price after disscount\n"))
+if choice ==1:
+    print ("The price of the item after discount is", powder)
+elif choice ==2:
+    print ("The price of the item after discount is", Eyeshadow = 720 * 10 / 100)
+elif choice ==3:
+    print ("The price of the item after discount is", lipstick)
+else:
+        print ("you have to choose from the list")
